87d6017e2bc78a44d25a52a8e48d359198a464f2
Benjamin Soltoff <ben@bensoltoff.com>
2020-06-24 19:43:14 -0500

    Revise lego-map.R to include all of Antarctica and clean up the code/image rendering

--- lego-map.R
+++ lego-map.R
@@ -4,15 +4,17 @@ library(tidyverse)
 library(png) # image handler
 library(viridis) # for color pallete
 library(scales) # scales
-library(magick) # making gif
+# library(magick) # making gif
 library(rayshader) # for all the fun
 library(here)
+
+theme_set(theme_void())
 
 # read image
 img <- png::readPNG(here("data", "mapa LEGO 50 X 50.png"))
 
 # create the matrix for the whole map
-world.matrix <- matrix(data = NA, nrow = 84, ncol = 192)
+world.matrix <- matrix(data = NA, nrow = 96, ncol = 192)
 
 # populate a matrix
 for (i in 0:(nrow(world.matrix) - 1)) { # iterates rows
@@ -28,40 +30,56 @@ for (i in 0:(nrow(world.matrix) - 1)) { # iterates rows
 # colors become numbers
 world.matrix[world.matrix == "10105174"] <- 1 # for light blue
 world.matrix[world.matrix == "108152201"] <- 2 # for dark blue
-world.matrix[world.matrix == "5013173"] <- 3 # for light greeb
-world.matrix[world.matrix == "253256252"] <- 3 # for white, for now, white would be light green
+world.matrix[world.matrix == "5013173"] <- 3 # for light green
 world.matrix[world.matrix == "16518869"] <- 4 # for dark green
 world.matrix[world.matrix == "24420641"] <- 5 # yellow
 world.matrix[world.matrix == "16310053"] <- 6 # blow light
 world.matrix[world.matrix == "1056439"] <- 7 # brown
+world.matrix[world.matrix == "253256252"] <- 8 # for white\
+world.matrix[world.matrix == "256256256"] <- 8 # white
+world.matrix[world.matrix == "1964027"] <- 9 # red
 
-# tranform in a data frame, and take last row line and last column line out (they were gray... I dont know, lol).
-d <- as.data.frame(world.matrix[1:83, 1:191])
+# transform in a data frame, and take last row line and last column line out
+d <- as_tibble(world.matrix[1:95, 1:191]) %>%
+  # add row numbers
+  mutate(row = -row_number()) %>%
+  # gather all the columns
+  pivot_longer(cols = c(-row),
+               names_to = "column",
+               values_to = "value",
+               names_transform = list(column = parse_number),
+               values_transform = list(value = parse_number))
 
-# names in the colums
-colnames(d) <- c(paste(rep("V", 190), 100:289, sep = ""))
-
-# add a column with row names, now stating in 11
-d$Row <- c(paste(rep("R", 83), 182:100, sep = ""))
-
-# gather
-d <- tidyr::gather(d, "Hight", "Value", 1:190)
-d$Value <- as.numeric(d$Value)
+# need to manually fix some of the interpolation
+d <- d %>%
+  mutate(
+    # bottom row should be all white
+    value = ifelse(row == min(row), 8, value),
+    # get rid of legend and replace with all white
+    value = ifelse(row <= -86 & column > 130 & column < 167, 8, value),
+    # fix greenland to be ice covered
+    value = ifelse(column > 47 & column < 100 & row > -20 & value == 6, 8, value),
+    # fix last column white squares which recorded latitude
+    value = ifelse(column == max(column) & row > -90 & value == 8, 1, value)
+    )
 
 # create graph
-g <- ggplot2::ggplot(d, aes(y = Row, x = Hight, fill = Value)) +
+g <- ggplot(d, aes(x = column, y = row, fill = value)) +
   geom_tile(color = "gray") +
-  coord_equal() + # set each tile to be squared, not rectangular
+  # set each tile to be squared, not rectangular
+  coord_equal(expand = FALSE, xlim = c(0, 192), ylim = c(-96, 0)) +
   labs(x = NULL, y = NULL) + # take out the axis legends
   scale_fill_gradientn(
-    colours = c(
+    colors = c(
       "#6C98C9",
       "#0A69AE",
       "#A5BC45",
       "#328349",
       "#F4CE29",
       "#A36435",
-      "#694027"
+      "#694027",
+      "#FFFFFF",
+      "#C4281B"
     ),
     values = rescale(c(1, 2, 3, 4, 5, 6, 7)),
     guide = "colorbar"
@@ -69,19 +87,14 @@ g <- ggplot2::ggplot(d, aes(y = Row, x = Hight, fill = Value)) +
 
 # Theme, margins manually edited
 g1 <- g + theme(
-  axis.ticks = element_blank(),
-  axis.text.x = element_blank(),
-  axis.text.y = element_blank(),
-  axis.title.x = element_blank(),
-  axis.title.y = element_blank(),
   plot.margin = unit(c(0, 0, 0, 0), "cm"),
   legend.position = "none"
 )
 g1
 
 # save output
-ggsave(filename = here("output", "lego-map.svg"), plot = g1, width = 10, height = 4.41, units = "in")
-ggsave(filename = here("output", "lego-map.png"), plot = g1, width = 10, height = 4.41, units = "in")
+ggsave(filename = here("output", "lego-map.svg"), plot = g1, width = 10, height = 5, units = "in")
+ggsave(filename = here("output", "lego-map.png"), plot = g1, width = 10, height = 5, units = "in")
 
 # experiment ray
 plot_gg(g1, width = 10, height = 4.41, scale = 30, offset_edges = TRUE)
